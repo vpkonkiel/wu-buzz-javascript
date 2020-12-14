ad303c53c41b161276dd8d73760b4f1bb88a7cfa
Victoria Konkiel <victoria.konkiel@yahoo.com>
2020-12-14 10:14:29 -0500

    completed test

--- WuBuzz.js
+++ WuBuzz.js
@@ -1,6 +1,19 @@
 class WuBuzz {
     wuChant(value){
-        return null;
+        let output = "";
+        for(let i = 1; i <= value; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                output += "WuTangForever";
+            } else if (i % 3 == 0) {
+                output += "Wu";
+            } else if (i % 5 == 0) {
+                output += "Tang"
+            } else {
+                output += i;
+            }
+            output += "\n"
+        }
+        return output;
     }
 }
 
